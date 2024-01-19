ea1778e9f72da2861ab1f3941230e5a2aa21ab53
Eric Mignot <eric.mignot@gmail.com>
2024-01-19 15:23:37 -0800

    ruby ready

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM ruby:3.3.0-alpine3.19
+
+WORKDIR /usr/src/app
+
+COPY . .
+
---
+++ ruby-ready.rb
@@ -0,0 +1,8 @@
+require 'test/unit'
+
+class TddReadyTest < Test::Unit::TestCase
+
+  def test_environment_is_ready
+    assert(true);
+  end
+end
