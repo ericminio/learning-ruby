6557fab1d0ba411b578aa510f5e2ea786417b839
Eric Mignot <eric.mignot@gmail.com>
2024-02-06 16:21:16 -0800

    more destructuring

--- about/tuple/destructuring.rb
+++ about/tuple/destructuring.rb
@@ -9,5 +9,11 @@ class DestructuringTest < Test::Unit::TestCase
     assert_equal(b, 2)
   end
 
+  def test_destructuring_can_populate_first_value_only
+    a, = 1, 2
+
+    assert_equal(a, 1)
+  end
+
   
 end
