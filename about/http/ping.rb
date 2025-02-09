a7b4c3e1a03f39f16a29539b869c260f74a334ce
ericminio <eric.mignot@gmail.com>
2025-02-09 13:16:42 -0800

    ping

---
+++ about/http/ping.rb
@@ -0,0 +1,27 @@
+require 'test/unit'
+require_relative './server'
+require 'net/http'
+require 'json'
+
+class PingTest < Test::Unit::TestCase
+
+  def setup
+    @server = Server.new 5001
+    Thread.new { 
+      @server.start 
+    }
+    @uri = URI('http://localhost:5001')
+  end
+
+  def teardown
+  end
+
+  def test_ping
+    response = Net::HTTP.get_response(@uri)
+    answer = JSON.parse(response.body)
+
+    assert_equal(answer, { "alive" => true })
+  end
+
+  
+end
---
+++ about/http/server.rb
@@ -0,0 +1,27 @@
+require 'socket'
+
+class Server
+    
+    def initialize(port)
+        @port = port
+        @server = TCPServer.new port
+        puts 'Server created on port ' + @port.to_s
+    end
+    def start
+        puts 'Server is listening...'
+        body = "{\"alive\":true}"            
+        while session = @server.accept
+            request = session.gets
+            puts request
+
+            unless request.nil?
+                session.print "HTTP/1.1 200\r\n"
+                session.print "Content-Type: application/json\r\n"
+                session.print "Content-Length: " + body.length.to_s + "\r\n"
+                session.print "\r\n"
+                session.print body
+            end
+            session.close
+        end
+    end
+end
