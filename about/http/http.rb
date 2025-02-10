a9e6809214af107824b5ceca167bb927447033fb
ericminio <eric.mignot@gmail.com>
2025-02-09 21:32:07 -0800

    http

--- about/http/http.rb
+++ about/http/http.rb
@@ -6,7 +6,7 @@ require 'json'
 class PingTest < Test::Unit::TestCase
 
   def setup
-    @server = Server.new(5001, { "alive" => true })
+    @server = Server.new(5001, ->(form) { {"alive" => true} } )
     @server_thread = Thread.new {  @server.listen_once }
     @uri = URI('http://localhost:5001')
   end
@@ -15,20 +15,27 @@ class PingTest < Test::Unit::TestCase
     @server_thread.exit
   end
 
-  def test_ping
+  def test_answer_set_at_initialization
     response = Net::HTTP.get_response(@uri)
     answer = JSON.parse(response.body)
 
     assert_equal(answer, { "alive" => true })
   end
 
-  def test_modify_answer
-    @server.answer = { "message" => "hello world" }
+  def test_answer_set_later
+    @server.answer = ->(form) { { "message" => "hello world" } }
     response = Net::HTTP.get_response(@uri)
     answer = JSON.parse(response.body)
 
     assert_equal(answer, { "message" => "hello world" })
   end
 
+  def test_dynamic_answer
+    @server.answer = ->(form) { { incoming:form } }
+    response = Net::HTTP.post_form(@uri, 'one' => 1, 'two' => '2' )
+    answer = JSON.parse(response.body)
+
+    assert_equal(answer, { "incoming" => "one=1&two=2" })
+  end
   
 end
--- about/http/server.rb
+++ about/http/server.rb
@@ -13,14 +13,28 @@ class Server
     def listen_once
         session = @server.accept
         request = session.gets
+        incoming = read_body(session)              
         unless request.nil?
+            body = @answer.(incoming).to_json
             session.print "HTTP/1.1 200\r\n"
             session.print "Content-Type: application/json\r\n"
-            session.print "Content-Length: " + @answer.to_json.length.to_s + "\r\n"
+            session.print "Content-Length: " + body.length.to_s + "\r\n"
             session.print "\r\n"
-            session.print @answer.to_json
+            session.print body
         end
         session.close    
-        @server.close    
+        @server.close
+        sleep(0.15)
+    end
+
+    def read_body(socket)
+      content_length = 0
+      while (line = socket.gets)
+        break if line.strip.empty?
+        key, value = line.split(': ', 2)
+        content_length = value.to_i if key == 'Content-Length'        
+      end
+      return unless content_length > 0
+      socket.read(content_length)
     end
 end
